2b5f747ec2c9ff9ae5add45f48ac77b3cbc89360
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-03-05 17:32:34 +0100

    prepare to rebase

--- src/main/java/praktikum/Main.java
+++ src/main/java/praktikum/Main.java
@@ -3,6 +3,7 @@ package praktikum;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main method");
-	System.out.println("Merge conflict - never again!");
+        System.out.println("No conflict");
+        System.out.println("Merge conflict - never again!");
     }
 }
